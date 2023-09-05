f68a3a3d96ad7c589552179685771a6931bc3514
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-09-05 13:30:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
            session_builder(conn)  # Pass 'conn' to session_builder
     
     elif selected_page == "CONTEXT":
-        role_selection(session)  # Call the function from role_selection.py module
+        role_selection(Session)  # Call the function from role_selection.py module
     
    
 if __name__ == "__main__":
