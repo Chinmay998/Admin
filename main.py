85bf8d0f8cc3697c5532fbd5bd79bb896900677e
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-09-05 13:22:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def main():
     page_options = ["Log_in", "CONTEXT"]  # Add more pages here
     selected_page = st.sidebar.selectbox("Select a Page", page_options)
 
-       if selected_page == "Login":
-        conn = sfAccount_selector()  # Get 'conn' from sfAccount_selector
-        if conn is not None:
-            session_builder(conn)  # Pass 'conn' to session_builder
+   if selected_page == "Login":
+    conn = sfAccount_selector()  # Get 'conn' from sfAccount_selector
+    if conn is not None:
+        session_builder(conn)  # Pass 'conn' to session_builder
     
     elif selected_page == "CONTEXT":
         role_selection()  # Call the function from role_selection.py module
