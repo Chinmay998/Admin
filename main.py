f9f64bc06c2e95117cce3cb4baf4a420f6c370aa
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-09-06 10:54:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ from Context import role_selection
 
 
 st.title('Multi-Page App')
+config = configparser.ConfigParser()
+config.sections()
+config.read('config.ini')
 
 def main():
     page_options = ["Log_in", "CONTEXT"]  # Add more pages here
     selected_page = st.sidebar.selectbox("Select a Page", page_options)
 
     if selected_page == "Login":
-        conn = sfAccount_selector()  # Get 'conn' from sfAccount_selector
+        conn = sfAccount_selector(config)  # Get 'conn' from sfAccount_selector
         if conn is not None:
-           session_builder(conn)  # Pass 'conn' to session_builder
+            session = Session.builder.configs(conn).create()  # Pass 'conn' to session_builder
     
     elif selected_page == "CONTEXT":
         role_selection(Session)  # Call the function from role_selection.py module
